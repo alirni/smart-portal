2476347f16b461bb711c4818abf91c0e3d1395ff
alirni <aliramezani19.1@gmail.com>
2025-03-12 02:22:04 +0330

    feat: add types

---
+++ src/types/index.ts
@@ -0,0 +1,34 @@
+export interface FormStructure {
+  formId: string;
+  fields: FormStructureSection[];
+  title: string;
+}
+
+export interface FormStructureSection {
+  fields: FormStructureFields[];
+  id: string;
+  label: string;
+  type: string;
+}
+
+export interface FormStructureFields {
+  id: string;
+  label: string;
+  required: boolean;
+  type: string;
+  options?: string[];
+  dynamicOptions?: DynamicOptions;
+  visibility?: Visibility;
+}
+
+export interface DynamicOptions {
+  dependsOn: string;
+  endpoint: string;
+  method: string;
+};
+
+export interface Visibility {
+  condition: "equals" | "notEquals";
+  dependsOn: string;
+  value: string;
+};
