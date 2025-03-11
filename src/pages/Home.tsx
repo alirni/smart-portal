4dd4eae55ad15dd58fb2688d2309abff3bd00f43
alirni <aliramezani19.1@gmail.com>
2025-03-12 02:19:26 +0330

    feat: home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,41 @@
+import { FC } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { Card, Button, Spin } from "antd";
+import { useNavigate } from "react-router-dom";
+import { fetchForms } from "../api/formService";
+import { FormStructure } from "../types";
+
+export const Home: FC = () => {
+  const { data: forms, isLoading } = useQuery({
+    queryKey: ["forms"],
+    queryFn: fetchForms,
+  });
+
+  const navigate = useNavigate();
+
+  if (isLoading) return <Spin size="large" />;
+
+  return (
+    <div
+      style={{
+        display: "grid",
+        gap: "20px",
+        maxWidth: "600px",
+        margin: "auto",
+      }}
+    >
+      {forms.map((form: FormStructure) => (
+        <Card key={form.formId} title={form.title}>
+          <Button
+            type="primary"
+            onClick={() => navigate(`/form/${form.formId}`)}
+          >
+            Fill Out Form
+          </Button>
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
